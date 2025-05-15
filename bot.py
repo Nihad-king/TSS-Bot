a99b0a68e042ace37ea304e9614747410ae1f929
Nihad-king <68867801+Nihad-king@users.noreply.github.com>
2025-05-15 21:38:59 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import os
 
-from activity_tracker import setup as setup_tracker
 from activity_check import setup_activity_check
 
 intents = discord.Intents.default()
@@ -16,7 +15,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # Server- und Channel-IDs
 GUILD_ID = 1167097735133016106
 LOG_CHANNEL_ID = 1310346537381007370
-INFO_CHANNEL_ID = 1167168385180782754
 TOP_ROLE_NAME = "Real Active God"
 
 @bot.event
@@ -27,7 +25,6 @@ async def on_ready():
     if log_channel:
         await log_channel.send(f"🟢 {bot.user.name} ist jetzt online!")
 
-    setup_tracker(bot, GUILD_ID, TOP_ROLE_NAME, INFO_CHANNEL_ID)
-    setup_activity_check(bot)
+    setup_activity_check(bot) 
 
 bot.run(os.getenv("TOKEN"))
